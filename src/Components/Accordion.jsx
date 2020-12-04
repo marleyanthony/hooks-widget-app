d032826a128a0a4862ebed1e5f95169d2097f61c
Marley Anthony <62618323+marleyanthony@users.noreply.github.com>
2020-12-04 10:08:58 -0800

    initial commit. file structure and basic jsx

---
+++ src/Components/Accordion.jsx
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+const Accordion = ({ items }) => {
+   const renderedItems = items.map(item => {
+      return (
+         <div key={item.title}>
+            <div className="title active">
+               <i className="dropdown icon"></i>
+               {item.title}
+            </div>
+            <div className="content active">
+               <p>{item.content}</p>
+            </div>
+         </div>
+      )
+   });
+
+   return (
+      <div className="ui style accordion">
+         {renderedItems}
+      </div>
+   )
+}
+
+export default Accordion
